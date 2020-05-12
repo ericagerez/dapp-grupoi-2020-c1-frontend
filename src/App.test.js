ba32bd1a15199a96a9422fa1c91f9c57609f58cf
Pablo <pablopissi@gmail.com>
2020-05-11 23:17:54 -0300

    Agrego test para validar la URL de la api

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { API_URL } from './api/base';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Bienvenidx/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('api url is correct', () => {
+  expect(API_URL).toEqual('https://compramundo-admin.herokuapp.com/api/')
+})
